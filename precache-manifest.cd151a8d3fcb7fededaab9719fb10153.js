ffa0a9226ad553d942605e339f3804fb8621a28e
mukhlisadhe <adhemukhlis@gmail.com>
2019-02-15 03:25:59 +0700

    #2
    ke 2

---
+++ precache-manifest.cd151a8d3fcb7fededaab9719fb10153.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f214c585872a242dfab9",
+    "url": "/static/js/main.f214c585.chunk.js"
+  },
+  {
+    "revision": "7b542fd54fe48a4fcf08",
+    "url": "/static/js/1.7b542fd5.chunk.js"
+  },
+  {
+    "revision": "f214c585872a242dfab9",
+    "url": "/static/css/main.c40ca66c.chunk.css"
+  },
+  {
+    "revision": "a5b5fe9fac6628083ea141565c9f6d9a",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1f90165a76d0fdc8ee226d6e38ed969e.js"
+  "/precache-manifest.cd151a8d3fcb7fededaab9719fb10153.js"
 );
 
 workbox.clientsClaim();
